034ea3ef34fe2057ee2baff35247ebb80e407e0a
Chris Parsons <chris.parsons@thegoodagency.co.uk>
2014-10-15 17:04:05 +0100

    projects pages/ updates

--- js/custom.js
+++ js/custom.js
@@ -194,3 +194,10 @@ jQuery(function($) {
 		}		            
     });
 });
+$(function(){
+    $("#big-image img:eq(0)").nextAll().hide();
+    $(".small-images img").click(function(e){
+        var index = $(this).index();
+        $("#big-image img").eq(index).show().siblings().hide();
+    });
+});
